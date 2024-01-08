c9bc6a442cc35bacfd43a953ec7488d9da8ea59f
ljkeller <kellerjlucas@gmail.com>
2024-01-08 15:06:50 -0600

    animal_detection

--- integrate.sh
+++ integrate.sh
@@ -5,68 +5,59 @@ echo "Usage (inside rzboard_app_zoo dir): ./integrate.sh"
 DEMO_DIR="/home/root/demos"
 BASE_DIR=$(pwd)
 
-# Function for Age and Gender Detection
 function age_gender_detection {
     (
-        echo "Running Age and Gender Detection..."
+        echo "Integrating Age and Gender Detection..."
 
         cd "./age_gender_detection/app" || exit
         echo "make"
         cd "../" || exit
 
-        mkdir -p "$DEMO_DIR/foo"
-        cp -R "./app/." "$DEMO_DIR/foo"
+        mkdir -p "$DEMO_DIR/age_gender_detection"
+        cp -R "./app/." "$DEMO_DIR/age_gender_detection"
     )
-    # Add commands specific to age_gender_detection
 }
 
-# Function for Animal Detection
 function animal_detection {
-    echo "Running Animal Detection..."
-    # Add commands specific to animal_detection
+    (
+        echo "Integrating Animal Detection..."
+
+        cd "./animal_detection/usbcam_app" || exit
+        echo "make"
+        cd "../" || exit
+
+        mkdir -p "$DEMO_DIR/animal_detection"
+        cp -R "./usbcam_app/." "$DEMO_DIR/animal_detection"
+    )
 }
 
-# Function for Driver Monitoring System
 function driver_monitoring_system {
-    echo "Running Driver Monitoring System..."
-    # Add commands specific to driver_monitoring_system
+    echo "Integrating Driver Monitoring System..."
 }
 
-# Function for Elderly Fall Detection
 function elderly_fall_detection {
-    echo "Running Elderly Fall Detection..."
-    # Add commands specific to elderly_fall_detection
+    echo "Integrating Elderly Fall Detection..."
 }
 
-# Function for Hand Gesture Recognition
 function hand_gesture_recognition {
-    echo "Running Hand Gesture Recognition..."
-    # Add commands specific to hand_gesture_recognition
+    echo "Integrating Hand Gesture Recognition..."
 }
 
-# Function for Head Counting
 function head_counting {
-    echo "Running Head Counting..."
-    # Add commands specific to head_counting
+    echo "Integrating Head Counting..."
 }
 
-# Function for Human Gaze Detection
 function human_gaze_detection {
-    echo "Running Human Gaze Detection..."
-    # Add commands specific to human_gaze_detection
+    echo "Integrating Human Gaze Detection..."
 }
 
-# Function for Safety Helmet and Vest Detection
 function safety_helmet_vest_detection {
-    echo "Running Safety Helmet and Vest Detection..."
-    # Add commands specific to safety_helmet_vest_detection
+    echo "Integrating Safety Helmet and Vest Detection..."
 }
 
-# Main integration function
 function integrate_applications {
     echo "Integrating applications..."
 
-    # Call each function
     age_gender_detection
     animal_detection
     driver_monitoring_system
@@ -79,5 +70,4 @@ function integrate_applications {
     echo "Integration complete."
 }
 
-# Execute the main function
 integrate_applications
